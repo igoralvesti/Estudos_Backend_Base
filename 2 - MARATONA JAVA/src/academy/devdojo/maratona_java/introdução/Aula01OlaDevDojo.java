45657d84db94620e67f21316ef2217c27120a620
Igor Alves <igoralvesbastos011@gmail.com>
2026-02-04 15:21:50 -0400

    commit inicial

--- 2 - MARATONA JAVA/src/academy/devdojo/maratona_java/introdução/Aula01OlaDevDojo.java
+++
@@ -1,7 +0,0 @@
-package academy.devdojo.maratona_java.introdução;
-
-public class Aula01OlaDevDojo {
-    public static void main(String[] args){
-        System.out.println("KA-ME-HA-ME-HA");
-    }
-}
