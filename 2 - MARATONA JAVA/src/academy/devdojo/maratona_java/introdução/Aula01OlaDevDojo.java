ac130aa4a2ddc1e63380a6683b9a2887fd29324b
Igor Alves <igoralvesbastos011@gmail.com>
2026-02-04 07:56:45 -0400

    docs(git): adiciona anotações sobre comandos básicos

---
+++ 2 - MARATONA JAVA/src/academy/devdojo/maratona_java/introdução/Aula01OlaDevDojo.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratona_java.introdução;
+
+public class Aula01OlaDevDojo {
+    public static void main(String[] args){
+        System.out.println("KA-ME-HA-ME-HA");
+    }
+}
---
+++ 2 - MARATONA JAVA/src/academy/devdojo/maratona_java/introdução/Aula02TiposPrimitivos.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratona_java.introdução;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String [] args) {
+        //int, double,float, char, byte, short, long, boolean
+        int idade = 10;
+        double salarioDouble = 2000;
+        long numeroGrande = 100000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShot = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041';
+
+        System.out. println ("A idade é "+idade+" anos");
+        System.out.println(falso);
+        System.out.println("char "+caractere);
+        System
+    }
+}
